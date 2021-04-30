383ca8105c2924dc021786efc0ec4ca59e7b70d6
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-04-30 17:22:38 -0300

    refactor view

--- task_view.rb
+++ task_view.rb
@@ -1,27 +1,26 @@
 class TaskView
 	def display(array)
-		puts "Tasks:"
-		puts "Index  --   Status   -- Title"
-		array.each_with_index do |task, index| 
-			status = task.done? ? "[X]" : "[ ]"
-			puts "• #{index + 1} - #{status} | #{task.title}"
+		CLI::UI::Frame.open('Tasks') do
+			puts "Index  --   Status   -- Title"
+			array.each_with_index do |task, index| 
+				status = task.done? ? "[X]" : "[ ]"
+				puts "• #{index + 1} - #{status} | #{task.title}"
+			end
 		end
 	end
 	
 	def ask_user_for(stuff)# Perguntar pro User titulo da Task
-		puts "#{stuff}?"
-		print ">"
-		gets.chomp
+		CLI::UI::Frame.open("#{stuff}") do
+			puts "#{stuff}?"
+			print ">"
+			gets.chomp
+		end
 	end
 
 	def print_actions
-		puts "\n----"
-		puts "What you wnat to do?"
-		puts "1. Add a new Task"
-		puts "2. List All Tasks"
-		puts "3. Mark Task as Done"
-		puts "4. Exit"
-		print "> "
-		gets.chomp.to_i
+		CLI::UI::Frame.open('Welcome to Task Manager') do
+			gets.chomp
+  		end		
 	end
+	
 end
