26c3b52609c201d5a8ec00e9e81ab9dacba3a22c
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-04-30 17:22:19 -0300

    refactor router

--- router.rb
+++ router.rb
@@ -1,20 +1,30 @@
+require 'cli/ui'
 
 class Router
 	def initialize(controller, view)
 	  @controller = controller
 	  @view = view
+	  @running = true
 	end
 
 	def run
-		loop do
-			choice = print_actions
-			dispatch(choice)
+		while @running
+			print_actions
 		end
 	end  # Rodar o programa
 	
 	def print_actions # Lista de Opções
-		@view.print_actions
+		CLI::UI::Prompt.ask('Welcome to Task Manager') do |handler|
+  			handler.option('Add a New Task')  { |selection| @controller.add }
+  			handler.option('List all Tasks')     { |selection| @controller.list }
+  			handler.option('Mark Task as Done')   { |selection| @controller.done }
+  			handler.option('Exit') 					{ |selection| stop }
+		end
 	end 
+
+	def stop
+		@running = false
+	end
 	
 	def dispatch(choice) # Dispachar escolha do user p/ controller
 		if choice == 1
